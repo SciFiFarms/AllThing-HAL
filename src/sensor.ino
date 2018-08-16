7afb4c16ea00d6d9f5439da03bc75a364c9dd205
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2018-08-16 16:11:35 -0600

    HAL: Homie implementation that allows custom settings to control nodes and sensors (#2)
    H.A.L. (Homie Assisting Life) is a Homie implementation that allows the JSON config file to dynamically setup and control HomieNodes.
    
    It does this by having the code for setting up, reading, and controlling a given sensor all ready present. Just put in the needed settings for a sensor, and it'll run the code for that sensor.
    
    The steps to add a sensor type are:
    
        Put needed #includes with the other #includes.
        Define pointers for HomieNodes, any sensor/switch objects, and a HomieSetting object.
        Configure HomieSetting object in setup()
        Add code to create nodes in setup(). You'll need to include the advertise commands here as well.
        Add code to setup sensors/switches, as well as setting the property in setupHandler().
        Finally, the code to actually read sensors/flip switches should go in the loopHandler().
    
    I'm using global pointers for managing the HomieNodes. My hope is that this will allow me to have many types of sensors supported, and all that gets added is a bit of code, the sensor library, and a pointer. That way I'm not putting HomieNodes into memory that aren't needed. If the node is actually needed, a HomieNode will get dynamically created. At least, that's the idea.
    
    Currently, I'm creating at least one HomieNode for every sensor/switch HAL supports. This seems fairly inefficient, and I figure there has to be a fairly low limit to how many HomieNodes an ESP8266 can support. I suspect it's more than the number of sensors an ESP8266 can physically support.
    I tried to dynamically create the HomieNodes based off of the config, but the HomieNodes have to be setup before Homie.setup() has ran, and the config isn't available until after Homie.setup() has ran. This shouldn't be a problem in Homie 2.1 as it sounds like I'll be able to call Homie.setupConfig() (or something like that) before calling Homie.setup().
    
    I added some libraries to the Dockerfile so that the building of firmware doesn't take as long. I tried to prebuild the firmware too, but I ran into issues with PlatformIO using the wrong version of Homie.
    
    * Rewrite HAL to better facilitate adding more sensors.
    
    * Create the /workespace/data/homie folder as part of the build.
    
    * Remove the Homie lib install from the Dockerfile. Was causing more problems than it was worth.
    
    * Have Homie config JSON dynamically set up the HomieNode objects and communications.
    
    * Return to using the master branch.

--- src/sensor.ino
+++ src/sensor.ino
@@ -1,47 +1,135 @@
 #include <Homie.h>
 //#include <GDBStub.h>
+//#include <HomieSetting.h>
 #include <Adafruit_Sensor.h>
 
-// DHT sensor consts/vars
-  #include "DHT.h"
-  #define DHTPIN     D2     // What pin we're connected to
-  #define DHTTYPE    DHT22  // Can be DHT11, DHT22 (AM2302), DHT21 (AM2301)
-  DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
-  
-  //DHT dht;
-
+// TODO: Refactor MEASURE_INTERVAL to be a setting that is retrieved.
 const int MEASURE_INTERVAL = 1; // How often to poll DHT22 for temperature and humidity
-
+// TODO: Refactor lastMeasureSent so that it is static rather than a global. 
 unsigned long lastMeasureSent = 0;
 
-HomieNode temperatureNode("temperature", "temperature");
-HomieNode humidityNode("humidity", "humidity");
+// The DHT Sensors
+#include "DHT.h"
+// Can be DHT11, DHT22 (AM2302), DHT21 (AM2301)
+DHT *dht11; // Initialize DHT sensor for normal 16mhz Arduino
+  HomieSetting<long> dht11Setting("dht11_pin", "Which pin to use.");  // id, description
+  HomieNode *dht11_temperatureNode;
+  HomieNode *dht11_humidityNode;
+DHT *dht21; // Initialize DHT sensor for normal 16mhz Arduino
+  HomieSetting<long> dht21Setting("dht21_pin", "Which pin to use.");  // id, description
+  HomieNode *dht21_temperatureNode;
+  HomieNode *dht21_humidityNode;
+DHT *dht22; // Initialize DHT sensor for normal 16mhz Arduino
+  HomieSetting<long> dht22Setting("dht22_pin", "Which pin to use.");  // id, description
+  HomieNode *dht22_temperatureNode;
+  HomieNode *dht22_humidityNode;
+
+// List of supported sensors and their IDs.
+#define SENSOR_DHT11_ID = 1
+#define SENSOR_DHT21_ID = 2 
+#define SENSOR_DHT22_ID = 3
+
+/*Sensors nodes[] = {
+  {DHT22_TEMPERATURE_ID, HomieNode temperatureNode("temperature", "temperature")},
+  {DHT22_HUMIDITY_ID, HomieNode humidityNode("humidity", "humidity")}
+};
+*/
+
 
 void loopHandler() {
   if (millis() - lastMeasureSent >= MEASURE_INTERVAL * 1000UL || lastMeasureSent == 0) {
-    float temperature = dht.readTemperature(); // Read temperature as Celsius
-    float humidity = dht.readHumidity(); // Read humidity as relative [0-100]%
-    if (isnan(temperature) || isnan(humidity)) {
-      Homie.getLogger() << F("Failed to read from DHT sensor!");
-    } else {
-      Homie.getLogger() << F("Temperature: ") << temperature << " °C" << endl;
-      Homie.getLogger() << F("Humidity   : ") << humidity << " %" << endl;
-      temperatureNode.setProperty("degrees").send(String(temperature));
-      humidityNode.setProperty("relative").send(String(humidity));
+    if(dht11Setting.wasProvided())
+    {
+      float temperature = dht11->readTemperature(); // Read temperature as Celsius
+      float humidity = dht11->readHumidity(); // Read humidity as relative [0-100]%
+      if (isnan(temperature) || isnan(humidity)) {
+        Homie.getLogger() << F("Failed to read from DHT sensor!");
+      } else {
+        Homie.getLogger() << F("Temperature: ") << temperature << " °C" << endl;
+        Homie.getLogger() << F("Humidity   : ") << humidity << " %" << endl;
+        dht11_temperatureNode->setProperty("degrees").send(String(temperature));
+        dht11_humidityNode->setProperty("relative").send(String(humidity));
+      }
+      lastMeasureSent = millis();
     }
-    lastMeasureSent = millis();
+    if(dht21Setting.wasProvided())
+    {
+      float temperature = dht21->readTemperature(); // Read temperature as Celsius
+      float humidity = dht21->readHumidity(); // Read humidity as relative [0-100]%
+      if (isnan(temperature) || isnan(humidity)) {
+        Homie.getLogger() << F("Failed to read from DHT sensor!");
+      } else {
+        Homie.getLogger() << F("Temperature: ") << temperature << " °C" << endl;
+        Homie.getLogger() << F("Humidity   : ") << humidity << " %" << endl;
+        dht21_temperatureNode->setProperty("degrees").send(String(temperature));
+        dht21_humidityNode->setProperty("relative").send(String(humidity));
+      }
+      lastMeasureSent = millis();
+    }
+    if(dht22Setting.wasProvided())
+    {
+      float temperature = dht22->readTemperature(); // Read temperature as Celsius
+      float humidity = dht22->readHumidity(); // Read humidity as relative [0-100]%
+      if (isnan(temperature) || isnan(humidity)) {
+        Homie.getLogger() << F("Failed to read from DHT sensor!");
+      } else {
+        Homie.getLogger() << F("Temperature: ") << temperature << " °C" << endl;
+        Homie.getLogger() << F("Humidity   : ") << humidity << " %" << endl;
+        dht22_temperatureNode->setProperty("degrees").send(String(temperature));
+        dht22_humidityNode->setProperty("relative").send(String(humidity));
+      }
+      lastMeasureSent = millis();
+    }
   }
 }
 
 void setupHandler() {
-  // Nodes part
-  temperatureNode.setProperty("unit").send("c");
-  humidityNode.setProperty("unit").send("%");
+  // TODO: See if I can move these into setup(). Or move more of setup here.
+  if(dht11Setting.wasProvided())
+  {
+    // Nodes part
+    dht11_temperatureNode->advertise("unit");
+    dht11_temperatureNode->advertise("degrees");
+    dht11_temperatureNode->setProperty("unit").send("c");
+    dht11_humidityNode->advertise("unit");
+    dht11_humidityNode->advertise("relative");
+    dht11_humidityNode->setProperty("unit").send("%");
 
-  // Hardware part
-  pinMode(DHTPIN, OUTPUT);
-  dht.begin();
-  Homie.getLogger() << "DHT " << DHTTYPE << " on pin " << DHTPIN << endl;
+    // Hardware part
+    dht11 = new DHT(dht11Setting.get(), DHT11); // Initialize DHT sensor for normal 16mhz Arduino
+    pinMode(dht11Setting.get(), OUTPUT);
+    dht11->begin();
+    Homie.getLogger() << "DHT11 on pin " << dht11Setting.get() << endl;
+  }
+  if(dht21Setting.wasProvided())
+  {
+    // Nodes part
+    dht21_temperatureNode->advertise("unit");
+    dht21_temperatureNode->advertise("degrees");
+    dht21_temperatureNode->setProperty("unit").send("c");
+    dht21_humidityNode->advertise("unit");
+    dht21_humidityNode->advertise("relative");
+    dht21_humidityNode->setProperty("unit").send("%");
+
+    // Hardware part
+    dht21 = new DHT(dht21Setting.get(), DHT21); // Initialize DHT sensor for normal 16mhz Arduino
+    pinMode(dht21Setting.get(), OUTPUT);
+    dht21->begin();
+    Homie.getLogger() << "DHT21 on pin " << dht21Setting.get() << endl;
+  }
+  if(dht22Setting.wasProvided())
+  {
+    // Next step: Figure out if these can live here or if they belong in setup().
+    // Nodes part
+    dht22_temperatureNode->setProperty("unit").send("c");
+    dht22_humidityNode->setProperty("unit").send("%");
+
+    // Hardware part
+    dht22 = new DHT(dht22Setting.get(), DHT22); // Initialize DHT sensor for normal 16mhz Arduino
+    pinMode(dht22Setting.get(), OUTPUT);
+    dht22->begin();
+    Homie.getLogger() << "DHT22 on pin " << dht22Setting.get() << endl;
+  }
 }
 
 void setup() {
@@ -49,16 +137,46 @@ void setup() {
   Homie.setLedPin(15, HIGH);
   //Homie.enableBuiltInLedIndicator(false);
   //dht.setup(2); // data pin 2
-  Serial << "HELLO" << endl;
+  //Serial << "HELLO" << endl;
 
-  Homie_setFirmware("boxbot", "1.0.0");
-  Homie.setSetupFunction(setupHandler).setLoopFunction(loopHandler);
+  Homie_setFirmware("hal", "1.0.0");
+  Homie.setSetupFunction(setupHandler);
+  Homie.setLoopFunction(loopHandler);
 
-  temperatureNode.advertise("unit");
-  temperatureNode.advertise("degrees");
+  // Load sensor settings
+  dht11Setting.setDefaultValue(0).setValidator([] (long candidate) {
+    return (candidate >= 0) && (candidate <= 100);
+  }); 
+  dht21Setting.setDefaultValue(0).setValidator([] (long candidate) {
+    return (candidate >= 0) && (candidate <= 100);
+  }); 
+  dht22Setting.setDefaultValue(0).setValidator([] (long candidate) {
+    return (candidate >= 0) && (candidate <= 100);
+  }); 
 
-  humidityNode.advertise("unit");
-  humidityNode.advertise("relative");
+  // TODO: I've commented out these if statements because as of homie 2.0.0,
+  // the setting objects aren't setup before Homie.setup() is called. In 2.1, 
+  // it sounds like I'll be able to call Homie.loadSettings() or something like
+  // that to initialise the setting objects.
+  //if(dht11Setting.wasProvided())
+  //{
+    dht11_temperatureNode = new HomieNode("dht11_temperature", "temperature");
+    dht11_humidityNode = new HomieNode("dht11_humidity", "humidity");
+  //}
+  //if(dht21Setting.wasProvided())
+  //{
+    dht21_temperatureNode = new HomieNode("dht21_temperature", "temperature");
+    dht21_humidityNode = new HomieNode("dht21_humidity", "humidity");
+  //}
+  //if(dht22Setting.wasProvided())
+  //{
+    dht22_temperatureNode = new HomieNode("dht22_temperature", "sensor");
+    dht22_humidityNode = new HomieNode("dht22_humidity", "sensor");
+    dht22_temperatureNode->advertise("unit");
+    dht22_temperatureNode->advertise("degrees");
+    dht22_humidityNode->advertise("unit");
+    dht22_humidityNode->advertise("relative");
+  //}
 
   Homie.setup();
 }
